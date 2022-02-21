203352dd4d67f506bf2945d3e4cdaeb3ec649e9e
Adam Lawson <dev@goshawk22.uk>
2022-02-21 09:42:41 +0000

    proton: Add support for FaceUnlock

--- apps/apps.mk
+++ apps/apps.mk
@@ -77,3 +77,14 @@ ifeq ($(WITH_FOSS),true)
 $(call inherit-product, vendor/foss/foss.mk)
 
 endif
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
